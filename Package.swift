ae7328113015140105203ba531e4d1180982f5bc
crane-hiromu <h.crane.t@gmail.com>
2022-12-22 14:52:54 +0900

    add package

--- Package.swift
+++ Package.swift
@@ -13,12 +13,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/manolofdez/AsyncBluetooth", "1.3.0"..<"2.0.0"),
+        .package(url: "https://github.com/michael94ellis/SwiftUIJoystick", "1.5.0"..<"2.0.0")
     ],
     targets: [
         .target(
             name: "DroidKit",
             dependencies: [
                 .product(name: "AsyncBluetooth", package: "AsyncBluetooth"),
+                .product(name: "SwiftUIJoystick", package: "SwiftUIJoystick")
             ]),
         .testTarget(
             name: "DroidKitTests",
